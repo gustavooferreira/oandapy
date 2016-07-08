b484f3bb599b761cbd9ba16d84f2722c8e906511
Gustavo Ferreira <gustavojcoferreira@gmail.com>
2016-07-08 04:12:56 +0100

    Rectified a small bug in exceptions file

--- oanda_api/exceptions.py
+++ oanda_api/exceptions.py
@@ -11,18 +11,19 @@ class OandaError(Exception):
     """Oanda error exception"""
 
     def __init__(self, status_code, error_response):
-        # Necessary because Oanda API is buggy
-        if "errorCode" in error_response:
-            code = error_response["errorCode"]
-        else:
-            code = ""
-
-        if "errorMessage" in error_response:
-            message = error_response["errorMessage"]
-        else:
-            message = ""
+        # Those checks are necessary because Oanda API is buggy
 
         if error_response:
+            if "errorCode" in error_response:
+                code = error_response["errorCode"]
+            else:
+                code = ""
+
+            if "errorMessage" in error_response:
+                message = error_response["errorMessage"]
+            else:
+                message = ""
+
             msg = "OANDA server returned [{0}] error code, and API returned " \
                 "[{1}] error code ({2})".format(status_code, code, message)
         else:
